4fe12b11597a5243c1012457cb650f53f74052f2
“cunnellp5” <philip.cunnell@colorado.edu>
2016-11-14 09:46:14 -0700

    skeletor_landingPage

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Lyrical_Drummer
 Q1 Project
+
+# Instructions
+Drum app coming soon...
+
+For this app you can use the keyboard to make a beat!
+
+Keys:
+r - t - y
+f - g - h
+v - b - n
+
+Lyrics will be available for you to use and sing/rap along to your beat with!
---
+++ app.js
@@ -0,0 +1,31 @@
+$(document).ready (function(){
+
+  // $.get('http://hipsterjesus.com/api/', function(data) {
+  //   console.log(data);
+  //             $('#content').html( data.text );
+  //         });
+
+  $.get('http://randomword.setgetgo.com/.php', function(data) {
+      console.log(data);
+
+});
+
+
+
+
+// function play(){
+//      var audio1 = document.getElementById("audio1");
+//      audio1.play();
+// }
+
+// function play(){
+//      var audio2 = document.getElementById("audio2");
+//      audio2.play();
+// }
+//
+// function play(){
+//      var audio = document.getElementById("audio3");
+//      audio3.play();
+// }
+
+})
